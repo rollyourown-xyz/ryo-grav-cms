4997ee2c7ab214b4f76a06021fd4c5a70a2d2518
xyzroller <xyzroller@rollyourown.xyz>
2021-04-06 18:49:13 +0200

    Added first terraform code for testing

---
+++ project-deployment/00-variables.tf
@@ -0,0 +1,25 @@
+# Read variables for deployment as local variables
+##################################################
+
+# Configuration file paths
+locals {
+  host_config_path    = yamldecode(file("${abspath(path.root)}/../configuration/config_path.yml"))["host_config_path"]
+  project_config_path = yamldecode(file("${abspath(path.root)}/../configuration/config_path.yml"))["project_config_path"]
+}
+
+# LXD variables
+locals {
+  lxd_remote_name               = yamldecode(file(local.host_config_path))["host_hostname"]
+  lxd_host_private_ipv4_address = yamldecode(file(local.host_config_path))["host_wireguard_address"]
+  lxd_host_public_ipv4_address  = yamldecode(file(local.host_config_path))["host_public_ip"]
+  lxd_core_trust_password       = yamldecode(file(local.host_config_path))["lxd_core_trust_password"]
+}
+
+
+# Input Variables
+#################
+
+variable "haproxy_image_version" {
+  description = "Version of the haproxy-dmz image to deploy"
+  type        = string
+}
---
+++ project-deployment/02-haproxy-dmz-service.tf
@@ -0,0 +1,24 @@
+# Deployment of DMZ HAProxy for all services
+############################################
+
+module "deploy-haproxy-dmz" {
+  source = "./modules/deploy-container-static-ip"
+
+  lxd_remote                 = local.lxd_remote_name
+  host_external_ipv4_address = local.lxd_host_public_ipv4_address
+  container_image            = join("-", [ "haproxy-dmz", var.haproxy_image_version ])
+  container_name             = "haproxy-dmz"
+  container_profiles         = ["default"]
+  container_network          = "lxd-dmz"
+  container_ipv4_address     = "10.10.10.10"
+  container_cloud-init_file  = "cloud-init/cloud-init-basic.yml"
+
+  container_proxies = [
+    {name = "proxy0", protocol = "tcp", listen = "80", connect = "80"},
+    {name = "proxy1", protocol = "tcp", listen = "443", connect = "443"},
+  ]
+
+  container_mounts = [
+    {name = "haproxy-ssl", host_path = "/var/container-directories/tls/haproxy", mount_path = "/etc/haproxy/ssl", mount_readonly = true}
+  ]
+}
