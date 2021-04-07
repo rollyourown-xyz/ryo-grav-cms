44ccc38a10ce01fd79ff0718af49f0823f8fea65
xyzroller <xyzroller@rollyourown.xyz>
2021-04-07 09:57:42 +0200

    Added cloud-init for certbot

--- project-deployment/03-certbot-service.tf
+++ project-deployment/03-certbot-service.tf
@@ -11,7 +11,7 @@ module "deploy-certbot" {
   container_name             = "certbot"
   container_profiles         = ["default"]
   container_network          = "lxd-fe"
-  container_cloud-init       = file("cloud-init/cloud-init-certbot-service.yml")
+  container_cloud-init       = file("cloud-init/cloud-init-certbot.yml")
   
   container_mounts = [
     {name = "certbot-tls", host_path = "/var/container-directories/tls", mount_path = "/var/tls", mount_readonly = false}
