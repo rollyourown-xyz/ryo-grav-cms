5880b93f90c1b8914f636af9693a24e2d24d5995
xyzroller <xyzroller@rollyourown.xyz>
2021-04-07 18:20:25 +0200

    Added terraform files for webserver

---
+++ project-deployment/04-webserver-service.tf
@@ -0,0 +1,17 @@
+# Deployment of Grav webserver
+##############################
+
+module "deploy-webserver" {
+  source = "./modules/deploy-container-dynamic-ip"
+  
+  lxd_remote                 = local.lxd_remote_name
+  container_image            = join("-", [ "webserver", var.image_version ])
+  container_name             = "webserver"
+  container_profiles         = ["default"]
+  container_network          = "lxd-fe"
+  container_cloud-init       = file("cloud-init/cloud-init-grav-bootstrap.yml")
+  
+  container_mounts = [
+    {name = "grav-user", host_path = "/var/container-directories/website/grav", mount_path = "/var/www/grav-admin/user", mount_readonly = false}
+  ]
+}
