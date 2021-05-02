2dc53af6317e7b064c51bbdf78ac40e45e9e33da
xyzroller <xyzroller@rollyourown.xyz>
2021-05-02 15:22:44 +0200

    Host-setup script update for host setup separation

--- host-setup.sh
+++ host-setup.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
-ansible-playbook -i configuration/inventory host-setup/master.yml
+
+echo "Running host setup playbooks"
+echo ""
+
+# Generic host setup
+echo "Executing generic host setup playbooks"
+ansible-playbook -i configuration/inventory modules/ryo-host-setup-generic/host-setup/master.yml
+
+# Project-specific host setup
+echo "Executing project-specific host setup playbooks"
+ansible-playbook -i configuration/inventory host-setup-project/master.yml
