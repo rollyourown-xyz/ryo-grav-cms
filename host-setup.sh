22ec34eab3d9c23c47d366f62c8b610cd6d1c93b
xyzroller <xyzroller@rollyourown.xyz>
2021-05-14 07:31:28 +0200

    Added module-specific host setup to script

--- host-setup.sh
+++ host-setup.sh
@@ -7,6 +7,14 @@ echo ""
 echo "Executing generic host setup playbooks"
 ansible-playbook -i configuration/inventory modules/ryo-host-setup-generic/host-setup/master.yml
 
+# Module-specific host setup for ryo-service-registry-kv-store
+echo "Executing module-specific host setup playbooks for ryo-service-registry-kv-store"
+ansible-playbook -i configuration/inventory modules/ryo-service-registry-kv-store/host-setup-module/master.yml
+
+# Module-specific host setup for ryo-loadbalancer-tls-proxy
+echo "Executing module-specific host setup playbooks for ryo-loadbalancer-tls-proxy"
+ansible-playbook -i configuration/inventory modules/ryo-loadbalancer-tls-proxy/host-setup-module/master.yml
+
 # Project-specific host setup
 echo "Executing project-specific host setup playbooks"
 ansible-playbook -i configuration/inventory host-setup-project/master.yml
