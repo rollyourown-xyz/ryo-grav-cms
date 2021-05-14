273362c063fe7016e1d93e40dc497e5276161a57
xyzroller <xyzroller@rollyourown.xyz>
2021-05-14 07:29:25 +0200

    Added modules to get-modules script

--- get-modules.sh
+++ get-modules.sh
@@ -7,9 +7,10 @@ echo ""
 echo "Executing 'git submodule add' for ryo-host-setup-generic repository"
 git submodule add https://github.com/rollyourown-xyz/ryo-host-setup-generic modules/ryo-host-setup-generic
 
-## Project specific submodules
-#echo "Executing 'git submodule add' for <PROJECT MODULE> repository"
-#git submodule add https://github.com/rollyourown-xyz/<PROJECT MODULE> modules/<PROJECT MODULE>
+## Service Registry / Key-Value Store module
+echo "Executing 'git submodule add' for ryo-service-registry-kv-store repository"
+git submodule add https://github.com/rollyourown-xyz/ryo-service-registry-kv-store modules/ryo-service-registry-kv-store
 
-#echo "Executing 'git submodule add' for <PROJECT MODULE> repository"
-#git submodule add https://github.com/rollyourown-xyz/<PROJECT MODULE> modules/<PROJECT MODULE>
+## Loadbalancer / TLS Proxy module
+echo "Executing 'git submodule add' for ryo-loadbalancer-tls-proxy"
+git submodule add https://github.com/rollyourown-xyz/ryo-loadbalancer-tls-proxy modules/ryo-loadbalancer-tls-proxy
