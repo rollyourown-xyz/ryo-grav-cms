6866a4cafc07ab225a2ee8d348efc7fc0fcefa2a
xyzroller <xyzroller@rollyourown.xyz>
2021-05-15 16:31:41 +0200

    Changed TF for testing

--- project-deployment/04-webserver.tf
+++ project-deployment/04-webserver.tf
@@ -13,21 +13,21 @@
 # }
 
 
-## Deploy project service backends - !!! update image build to include consul service registration 
-# module "deploy-webserver" {
-#   source = "./modules/deploy-container-dynamic-ip"
+## Deploy project service backends 
+module "deploy-webserver" {
+  source = "./modules/deploy-container-dynamic-ip"
 
-#   lxd_remote                 = local.lxd_remote_name
-#   container_image            = join("-", [ local.project_id, "webserver", var.image_version ])
-#   container_name             = "webserver"
-#   container_profiles         = ["default"]
-#   container_network          = "lxd-fe"
-#   container_cloud-init       = file("cloud-init/cloud-init-grav-bootstrap.yml")
+  lxd_remote                 = local.lxd_remote_name
+  container_image            = join("-", [ local.project_id, "grav-webserver", var.image_version ])
+  container_name             = "grav-webserver"
+  container_profiles         = ["default"]
+  container_network          = local.project_id
+  container_cloud-init       = file("cloud-init/cloud-init-grav-bootstrap.yml")
   
-#   container_mounts = [
-#     {name = "grav-user", host_path = "/var/containers/ryo-grav-cms/website/grav", mount_path = "/var/www/grav-admin/user", mount_readonly = false}
-#   ]
-# }
+  container_mounts = [
+    {name = "grav-user", host_path = "/var/containers/ryo-grav-cms/website/grav", mount_path = "/var/www/grav-admin/user", mount_readonly = false}
+  ]
+}
 
 
 ## Configure HAProxy KVs for ACLs, Deny rules (?) and use-backend rules - depends on service backend deployment
