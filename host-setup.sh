8ae285ce0537f02fcbc4a09739428a746dc6cccb
xyzroller <xyzroller@rollyourown.xyz>
2021-05-21 07:07:59 +0200

    Changed master to main

--- host-setup.sh
+++ host-setup.sh
@@ -5,16 +5,16 @@ echo ""
 
 # Generic host setup
 echo "Executing generic host setup playbooks"
-ansible-playbook -i configuration/inventory modules/ryo-host-setup-generic/host-setup/master.yml
+ansible-playbook -i configuration/inventory modules/ryo-host-setup-generic/host-setup/main.yml
 
 # Module-specific host setup for ryo-service-registry-kv-store
 echo "Executing module-specific host setup playbooks for ryo-service-registry-kv-store"
-ansible-playbook -i configuration/inventory modules/ryo-service-registry-kv-store/host-setup-module/master.yml
+ansible-playbook -i configuration/inventory modules/ryo-service-registry-kv-store/host-setup-module/main.yml
 
 # Module-specific host setup for ryo-loadbalancer-tls-proxy
 echo "Executing module-specific host setup playbooks for ryo-loadbalancer-tls-proxy"
-ansible-playbook -i configuration/inventory modules/ryo-loadbalancer-tls-proxy/host-setup-module/master.yml
+ansible-playbook -i configuration/inventory modules/ryo-loadbalancer-tls-proxy/host-setup-module/main.yml
 
 # Project-specific host setup
 echo "Executing project-specific host setup playbooks"
-ansible-playbook -i configuration/inventory host-setup-project/master.yml
+ansible-playbook -i configuration/inventory host-setup-project/main.yml
