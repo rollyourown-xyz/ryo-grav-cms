fd9bd4be8e3bc082555f0b4f08d5fab913b272c5
xyzroller <xyzroller@rollyourown.xyz>
2021-05-23 11:49:03 +0200

    Modified LXD remote config for PKI-based auth

--- project-deployment/01-global-providers.tf
+++ project-deployment/01-global-providers.tf
@@ -16,16 +16,17 @@ terraform {
 }
 
 provider "lxd" {
-  # Remote configuration needed with LXD snap, even if remotes have been defined out of band via 'lxc remote add'
-  generate_client_certificates = true
-  accept_remote_certificate    = true
+
+  config_dir                   = "$HOME/snap/lxd/current/.config/lxc"
+  generate_client_certificates = false
+  accept_remote_certificate    = false
 
   lxd_remote {
     name     = local.lxd_remote_name
-    scheme   = "https"
-    address  = local.lxd_host_private_ipv4_address
-    port     = 8443
-    password = local.lxd_core_trust_password
+    #scheme   = "https"
+    #address  = local.lxd_host_private_ipv4_address
+    #port     = 8443
+    #password = local.lxd_core_trust_password
     default  = true
   }
 }
