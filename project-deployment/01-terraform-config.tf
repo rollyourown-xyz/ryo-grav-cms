51bb135d3542a412b588b9d95137a7d32e240d4e
xyzroller <xyzroller@rollyourown.xyz>
2021-08-09 21:45:43 +0200

    Updated terraform minimum required version to 0.15

--- project-deployment/01-terraform-config.tf
+++ project-deployment/01-terraform-config.tf
@@ -2,7 +2,7 @@
 ###########################################################################
 
 terraform {
-  required_version = ">= 0.14"
+  required_version = ">= 0.15"
   required_providers {
     lxd = {
       source  = "terraform-lxd/lxd"
