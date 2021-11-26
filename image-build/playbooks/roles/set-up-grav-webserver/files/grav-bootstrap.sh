5235290317aba9230b73b8c27885b4d96e808317
xyzroller <xyzroller@rollyourown.xyz>
2021-11-26 20:49:26 +0100

    Cleaned up removed code

--- image-build/playbooks/roles/set-up-grav-webserver/files/grav-bootstrap.sh
+++ image-build/playbooks/roles/set-up-grav-webserver/files/grav-bootstrap.sh
@@ -14,9 +14,3 @@ fi
 
 # Remove the grav user directory in the bootstrapping directory
 rm -R /usr/local/bootstrap/user
-
-# Copy cronjob file from update directory to /etc/cron.d/
-#cp -p /usr/local/bootstrap/grav_scheduler_cronjob /etc/cron.d/grav_scheduler_cronjob
-  
-# Restart cron service
-#/etc/init.d/cron restart
