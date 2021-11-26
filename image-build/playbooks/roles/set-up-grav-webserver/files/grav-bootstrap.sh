8327f1b281e56aa972ecc201fa7a5f02db061cd6
xyzroller <xyzroller@rollyourown.xyz>
2021-11-26 19:22:12 +0100

    Added grav scheduler cronjob to bootstrapping

--- image-build/playbooks/roles/set-up-grav-webserver/files/grav-bootstrap.sh
+++ image-build/playbooks/roles/set-up-grav-webserver/files/grav-bootstrap.sh
@@ -1,4 +1,4 @@
-# If the file "BOOTSRTAPPED" is not already present then run bootstrapping tasks
+# If the file "BOOTSTRAPPED" is not already present then run bootstrapping tasks
 if [ ! -f "/var/www/grav-admin/user/BOOTSTRAPPED" ]; then
 
   # Move the contents of the grav user directory into place
@@ -14,3 +14,9 @@ fi
 
 # Remove the grav user directory in the bootstrapping directory
 rm -R /usr/local/bootstrap/user
+
+# Copy cronjob file from update directory to /etc/cron.d/
+cp -p /usr/local/bootstrap/grav_scheduler_cronjob /etc/cron.d/grav_scheduler_cronjob
+  
+# Restart cron service
+/etc/init.d/cron restart
