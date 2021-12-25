b4cf8e22658292af93ef080c7bd82199c0b2283e
xyzroller <xyzroller@rollyourown.xyz>
2021-12-25 15:51:22 +0100

    Update for change of repository name

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 # This script deploys the modules required for the project and the project components 
 
 # Project ID
-PROJECT_ID="ryo-grav-cms"
+PROJECT_ID="ryo-grav-cms-standalone"
 
 # Required modules (space-separated list in the form "module_1 module_2 module_3")
 MODULES="ryo-service-proxy"
