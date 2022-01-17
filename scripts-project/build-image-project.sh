a9140ad062050b5e17128ec61cf577e374416530
xyzroller <xyzroller@rollyourown.xyz>
2022-01-17 17:44:45 +0100

    Bump Grav version to 1.7.27.1

--- scripts-project/build-image-project.sh
+++ scripts-project/build-image-project.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Default project software versions
-grav_version="1.7.25"
+grav_version="1.7.27.1"
 
 # Script directory
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
