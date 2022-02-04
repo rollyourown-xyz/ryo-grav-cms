90b340139fa0715d976ca43eeaeb8d9cd7419166
xyzroller <xyzroller@rollyourown.xyz>
2022-02-04 17:00:31 +0100

    Separate acl, deny and use-backend deployment

--- project-deployment/04-deploy-ingress-proxy-configuration.tf
+++ project-deployment/04-deploy-ingress-proxy-configuration.tf
@@ -9,7 +9,7 @@ module "deploy-grav-webserver-ingress-proxy-backend-service" {
   non_ssl_backend_services = [ join("-", [ local.project_id, "grav-webserver" ]) ]
 }
 
-module "deploy-grav-webserver-ingress-proxy-configuration" {
+module "deploy-grav-webserver-ingress-proxy-acl-configuration" {
   source = "../../ryo-ingress-proxy/module-deployment/modules/deploy-ingress-proxy-configuration"
 
   depends_on = [ module.deploy-grav-webserver-ingress-proxy-backend-service ]
@@ -17,6 +17,12 @@ module "deploy-grav-webserver-ingress-proxy-configuration" {
   ingress-proxy_host_only_acls = {
     join("-", [ local.project_id, "domain" ]) = {host = local.project_domain_name}
   }
+}
+
+module "deploy-grav-webserver-ingress-proxy-use-backend-configuration" {
+  source = "../../ryo-ingress-proxy/module-deployment/modules/deploy-ingress-proxy-configuration"
+
+  depends_on = [ module.deploy-grav-webserver-ingress-proxy-backend-service, module.deploy-grav-webserver-ingress-proxy-acl-configuration ]
 
   ingress-proxy_acl_use-backends = {
     join("-", [ local.project_id, "domain" ]) = {backend_service = join("-", [ local.project_id, "grav-webserver" ])}
