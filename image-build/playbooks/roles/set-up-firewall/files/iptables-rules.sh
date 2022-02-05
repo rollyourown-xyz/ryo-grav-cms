5fd00859cf391f43c436a6b80f0490ce880e0b45
xyzroller <xyzroller@rollyourown.xyz>
2022-02-05 08:30:50 +0100

    Move firewall setup to separate role

--- image-build/playbooks/roles/set-up-firewall/files/iptables-rules.sh
+++ image-build/playbooks/roles/set-up-firewall/files/iptables-rules.sh
@@ -21,6 +21,13 @@ $IPT -A INPUT -p icmp -m icmp --icmp-type 8 -j ACCEPT
 $IPT -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 
 
+## Map DNS port 53 to Consul listening port
+###########################################
+
+$IPT -t nat -A OUTPUT -d localhost -p udp -m udp --dport 53 -j REDIRECT --to-ports 8600
+$IPT -t nat -A OUTPUT -d localhost -p tcp -m tcp --dport 53 -j REDIRECT --to-ports 8600
+
+
 ## Consul Client Ports
 ######################
 
